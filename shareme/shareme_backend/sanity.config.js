90b4c11dee513c02ae5d99986739bc3e02ef6de3
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-04-02 00:00:32 -0600

    shareme app start

---
+++ shareme/shareme_backend/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'oldlace-raven',
+
+  projectId: 'e2z052jo',
+  dataset: 'shareme_jsm',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
