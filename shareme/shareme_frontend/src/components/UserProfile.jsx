320ac6fd47e53aaa7cee1713212341494f4f9f83
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-04-16 22:48:28 -0600

    user profile finish

--- shareme/shareme_frontend/src/components/UserProfile.jsx
+++ shareme/shareme_frontend/src/components/UserProfile.jsx
@@ -5,6 +5,7 @@ import { googleLogout } from '@react-oauth/google';
 
 import {
 	useCreatedPinsQuery,
+	userCreatedPinsQuery,
 	userQuery,
 	userSavedPinsQuery,
 } from '../utils/data';
@@ -14,6 +15,11 @@ import Spinner from './Spinner';
 
 const randomImage =
 	'https://source.unsplash.com/1600x900/?nature,photography,technology';
+
+const activeBtnStyles =
+	'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none';
+const notActiveBtnStyles =
+	'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none';
 
 const UserProfile = () => {
 	const [user, setUser] = useState(null);
@@ -30,6 +36,20 @@ const UserProfile = () => {
 			setUser(data[0]);
 		});
 	}, [userId]);
+
+	useEffect(() => {
+		if (text === 'Created') {
+			const createdPinsQuery = userCreatedPinsQuery(userId);
+			client.fetch(createdPinsQuery).then(data => {
+				setPins(data);
+			});
+		} else {
+			const savedPinsQuery = userSavedPinsQuery(userId);
+			client.fetch(savedPinsQuery).then(data => {
+				setPins(data);
+			});
+		}
+	}, [text, userId]);
 
 	const logout = () => {
 		localStorage.clear();
@@ -77,6 +97,42 @@ const UserProfile = () => {
 							)}
 						</div>
 					</div>
+					<div className='text-center mb-7'>
+						<button
+							type='button'
+							onClick={e => {
+								setText(e.target.textContent);
+								setActiveBtn('created');
+							}}
+							className={`${
+								activeBtn === 'created' ? activeBtnStyles : notActiveBtnStyles
+							}`}
+						>
+							Created
+						</button>
+						<button
+							type='button'
+							onClick={e => {
+								setText(e.target.textContent);
+								setActiveBtn('saved');
+							}}
+							className={`${
+								activeBtn === 'saved' ? activeBtnStyles : notActiveBtnStyles
+							}`}
+						>
+							Saved
+						</button>
+					</div>
+
+					{pins?.length ? (
+						<div className='px-2'>
+							<MasonryLayout pins={pins} />
+						</div>
+					) : (
+						<div className='flex justify-center font-bold items-center w-full text-xl mt-2'>
+							No Pins Found!
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
