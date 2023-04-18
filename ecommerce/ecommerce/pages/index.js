bed0d55ebb1e114323a13eb1a385808efb496c5e
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-04-18 17:15:18 -0600

    structure start

--- ecommerce/ecommerce/pages/index.js
+++ ecommerce/ecommerce/pages/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import { Product, FooterBanner, HeroBanner } from '../components';
+
 const Home = () => {
 	return (
-		<>
-			Hero Banner
-			<div>
+		<div>
+			<HeroBanner />
+
+			<div className='products-heading'>
 				<h2>Best Selling Products</h2>
 				<p>Speakers of many variations </p>
 			</div>
-			<div>{['Product 1', 'Product 2'].map(product => product)}</div>
-			Footer
-		</>
+			<div className='products-container'>
+				{['Product 1', 'Product 2'].map(product => product)}
+			</div>
+
+			<FooterBanner />
+		</div>
 	);
 };
 
