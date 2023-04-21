6971a684e929ed8689911c4ba0a5a93544b57c6c
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-04-21 15:18:50 -0600

    cart start

--- ecommerce/ecommerce/components/Navbar.jsx
+++ ecommerce/ecommerce/components/Navbar.jsx
@@ -3,8 +3,10 @@ import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
 
 import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+	const { showCart, setShowCart, totalQuantities } = useStateContext();
 	return (
 		<div className='navbar-container'>
 			<p className='logo'>
@@ -14,13 +16,13 @@ const Navbar = () => {
 			<button
 				type='button'
 				className='cart-icon'
-				onClick=''
+				onClick={() => setShowCart(true)}
 			>
 				<AiOutlineShopping />
-				<span className='cart-item-qty'>0</span>
+				<span className='cart-item-qty'>{totalQuantities}</span>
 			</button>
 
-			<Cart />
+			{showCart && <Cart />}
 		</div>
 	);
 };
