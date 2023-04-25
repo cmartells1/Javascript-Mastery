b531877904f4e88dbacfea145d314c616400aec8
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-04-25 16:06:35 -0600

    payment success page

--- ecommerce/ecommerce/context/StateContext.js
+++ ecommerce/ecommerce/context/StateContext.js
@@ -105,6 +105,9 @@ export const StateContext = ({ children }) => {
 				setShowCart,
 				toggleCartItemQuantity,
 				onRemove,
+				setCartItems,
+				setTotalPrice,
+				setTotalQuantities,
 			}}
 		>
 			{children}
